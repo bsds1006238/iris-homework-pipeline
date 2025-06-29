09f42ebc707ab731fd608fe36acf034468eedd2b
bsds1006238 <21f1006238@ds.study.iitm.ac.in>
2025-06-30 03:31:38 +0530

    Troubleshooting

--- test/test.py
+++ test/test.py
@@ -1,15 +1,21 @@
 from pipeline.load_data import load_data
 from pipeline.train import train_model
+import unittest
 from pipeline.evaluate import evaluate_model
 
 
-def test_data_shape():
-    X, y = load_data()
-    assert X.shape[0] == y.shape[0]
-    assert X.shape[1] == 4
+class TestEvaluation(unittest.TestCase):
+    def test_score_range(self):
+        model, X_test, y_test = train_model()
+        score = evaluate_model(model, X_test, y_test)
+        self.assertGreaterEqual(score, 0.0)
+        self.assertLessEqual(score, 1.0)
 
+    def test_model_accuracy(self):
+        model, X_test, y_test = train_model()
+        acc = evaluate_model(model, X_test, y_test)
+        self.assertGreater(acc, 0.8)
 
-def test_model_accuracy():
-    model, X_test, y_test = train_model()
-    acc = evaluate_model(model, X_test, y_test)
-    assert acc > 0.8
+if __name__ == '__main__':
+    unittest.main()
+
